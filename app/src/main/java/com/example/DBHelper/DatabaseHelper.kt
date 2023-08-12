5e0eba73f363c9108d35fe8b2221f77a575ef003
ChoiHeynho <chh0006@gmail.com>
2023-08-13 04:03:53 +0900

    Upload Project

--- app/src/main/java/com/example/DBHelper/DatabaseHelper.kt
+++ app/src/main/java/com/example/DBHelper/DatabaseHelper.kt
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper
 
 
 class DatabaseHelper private constructor(context : Context) :
-    SQLiteOpenHelper(context, DATABASE_NAME,null,DATABASE_VERSION){
+    SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION){
     companion object{
         //DB 이름, DB 버전, 테이블 이름
         const val DATABASE_NAME = "PlanList.db"
@@ -16,11 +16,12 @@ class DatabaseHelper private constructor(context : Context) :
 
         //DB에 저장할 데이터 속성
         const val COL1_ID = "_ID"
-        const val COL2_DATETIME = "DATETIME"
+        const val COL2_DATETIME = "_DATETIME"
         const val COL3_PLAN = "_PLAN" //나중에 ArrayList로 변경
 
 
 
+        //DB에 DBHelper인스턴스가 여러개 접근하면 문제가 발생할 수 있으므로 이를 방지하기 위해 싱글톤 패턴을 사용함
         //싱글톤 패턴을 위해 DatabaseHelper 생성자는 private으로 선언, getInstance내부에서 Double checked Locking을 통해서 인스턴스를 반환
         @Volatile
         private var instance : DatabaseHelper? = null
@@ -36,12 +37,12 @@ class DatabaseHelper private constructor(context : Context) :
 
     }
 
-    //프로그램에 데이터 베이스 파일이 없을 경우에만 단 1번 실행되는 메소드
+    //시스템에 데이터 베이스 파일이 없을 경우에만 단 1번 실행되는 메소드
     override fun onCreate(db: SQLiteDatabase?) {
         val createQuery : String = "CREATE TABLE $TABLE_NAME (" +
                 "$COL1_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "$COL2_DATETIME TEXT, " +
-                "$COL3_PLAN TEXT" +
+                "$COL2_DATETIME TEXT NOT NULL, " +
+                "$COL3_PLAN TEXT NOT NULL" +
                 ")"
 
         db?.execSQL(createQuery)
@@ -52,58 +53,51 @@ class DatabaseHelper private constructor(context : Context) :
     /*기능 : DB버전이 변경되었으면 기존 DB를 삭제하고 새로운 DB를 작성함*/
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         if(oldVersion != newVersion){
-            db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+            db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME;")
             onCreate(db)
         }
     }
 
 
-    //기능 : 테이블에 데이터 추가
+    //기능 : INSERT문을 사용해서 테이블에 일정을 추가한다.
     fun insertData(date: String, plan: String){
         val db : SQLiteDatabase = this.writableDatabase
-        val contentValues : ContentValues = ContentValues().apply{
-            put(COL2_DATETIME, date)
-            put(COL3_PLAN, plan)
-        }
-        db.insert(TABLE_NAME, null, contentValues)
+        //('와 ')은 특수문자(와 )를 사용한것이다.(영상 27:12 참고)
+        val insertQuery: String = "INSERT INTO $TABLE_NAME($COL2_DATETIME,$COL3_PLAN) VALUES('$date','$plan');"
+        db.execSQL(insertQuery)
     }
 
 
     /*기능 : id를 고유키로 갖고있는 데이터를 갱신*/
-    fun updateData(id: String, date: String, plan: String){
+    fun updateData(id: String, plan: String){
         val db : SQLiteDatabase = this.writableDatabase
-        val contentValues : ContentValues = ContentValues().apply {
-            put(COL2_DATETIME, date)
-            put(COL3_PLAN, plan)
-        }
-
-        db.update(TABLE_NAME, contentValues, "$COL1_ID = ?",arrayOf(id))//이 구문 뜻 알아보기
+        val updateQuery: String = "UPDATE $TABLE_NAME SET $COL3_PLAN = '$plan' WHERE $COL1_ID = '$id';"
+        db.execSQL(updateQuery)
     }
+
 
     /*기능 : id를 고유키로 갖고있는 데이터를 삭제*/
     fun deleteData(id: String){
         val db: SQLiteDatabase = this.writableDatabase
-        db.delete(TABLE_NAME, "$COL1_ID = ?", arrayOf(id))
+        val deleteQuery: String ="DELETE FROM $TABLE_NAME WHERE $COL1_ID = '$id';"
+        db.execSQL(deleteQuery)
     }
 
 
-
-
-    //아직 수정안됨
-    /*기능 : 모든 데이터를 출력*/
-    fun getAllData() : String{
+    /*기능 : date와 일치하는 데이터를 가져옴*/
+    fun getAllData(date: String) : String{
         var result = "No data in DB"
         val db : SQLiteDatabase = this.readableDatabase
-        val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+        //WHERE절에서 SQLite의 date함수를 호출해서 사용하도록 쿼리문을 구성하려고함
+        val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME WHERE date($COL2_DATETIME) = '$date';", null)
 
         try{
             if(cursor.count != 0){
                 val stringBuffer = StringBuffer()
                 while(cursor.moveToNext()){
                     stringBuffer.append("ID : " + cursor.getInt(0).toString()+"\n")
-                    stringBuffer.append("이름 : " + cursor.getString(1).toString()+"\n")
-                    stringBuffer.append("전화번호 : " + cursor.getString(2).toString()+"\n")
-                    stringBuffer.append("이메일 : " + cursor.getString(3).toString()+"\n\n")
+                    stringBuffer.append("날짜 : " + cursor.getString(1).toString()+"\n")
+                    stringBuffer.append("일정 : " + cursor.getString(2).toString()+"\n\n")
                 }
                 result = stringBuffer.toString()
             }
@@ -116,4 +110,5 @@ class DatabaseHelper private constructor(context : Context) :
         }
         return result
     }
+    
 }
--- app/src/main/java/com/example/checkplan/MainActivity.kt
+++ app/src/main/java/com/example/checkplan/MainActivity.kt
@@ -3,54 +3,146 @@ package com.example.checkplan
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import com.example.CustomView.CustomCalendar
+import com.example.DBHelper.DatabaseHelper
 import com.example.checkplan.databinding.ActivityMainBinding
 import com.example.checkplan.databinding.FragmentPlanManagementBinding
 import com.google.android.material.tabs.TabLayout
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val mainActivitiyBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        val tab1 : TabLayout.Tab = mainActivitiyBinding.tabs.newTab()
-        tab1.text = "계획 달성률"
-        mainActivitiyBinding.tabs.addTab(tab1)
-
-        val tab2 : TabLayout.Tab = mainActivitiyBinding.tabs.newTab()
-        tab2.text = "일정 관리"
-        mainActivitiyBinding.tabs.addTab(tab2)
-
-        val tab3 : TabLayout.Tab = mainActivitiyBinding.tabs.newTab()
-        tab3.text = "친구 관리"
-        mainActivitiyBinding.tabs.addTab(tab3)
-
-        //탭을 선택하기 전에 기본 뷰로 fragmentOne을 출력
-        supportFragmentManager.beginTransaction().replace(R.id.tabContent, PlanManagementFragment()).commit()
-        mainActivitiyBinding.tabs.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            //탭 버튼을 눌렀을때 동작
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val transaction = supportFragmentManager.beginTransaction()
-                transaction.addToBackStack(null)
-                when(tab?.text){
-                    "계획 달성률" -> transaction.replace(R.id.tabContent, PlanAchievementRateFragment())
-                    "일정 관리" -> transaction.replace(R.id.tabContent, PlanManagementFragment())
-                    "친구 관리" -> transaction.replace(R.id.tabContent, FriendManagementFragment())
-                }
-                transaction.commit()
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-
-        })
-        setContentView(mainActivitiyBinding.root)
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val databaseHelper: DatabaseHelper by lazy{
+        DatabaseHelper.getInstance(applicationContext)
+    }
+
+    //설명 : 액티비티 생성
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        //일정 관리 기능 초기화
+        addTodo()
+        removeTodo()
+        updateTodo()
+        getAllTodo()
+
+        val customCalendar = CustomCalendar(binding.customCalendar)
+            customCalendar.editCalendar()
+            customCalendar.applyDecorator()
+            customCalendar.setEndTimeCalendar()
+
+        //날짜 선택 기능 초기화
+        setSelectedDate()
+
+        setContentView(binding.root)
+    }
+
+
+    //설명 : 액티비티 소멸
+    override fun onDestroy() {
+        databaseHelper.close()
+        super.onDestroy()
+    }
+
+    //설명 : 날짜 선택 시 날짜를 출력, 이전 날짜를 선택 시 보기 버튼만 활성화, 오늘 혹은 이후 날짜 선택 시 모든 일정 관리 버튼 활성화
+    private fun setSelectedDate(){
+        /*widget은 MaterialCanendarView, date는 선택된 날짜, selected 날짜가 선택되었는지에 대한 상태*/
+        binding.customCalendar.setOnDateChangedListener { widget, date, selected ->
+            val localDateOf: LocalDate = LocalDate.of(date.year,date.month,date.day)
+            binding.outputDate.text = localDateOf.toString()
+            if(date.isBefore(CalendarDay.today())){
+                binding.insertTodoBtn.visibility = View.INVISIBLE
+                binding.deleteTodoBtn.visibility = View.INVISIBLE
+                binding.updateTodoBtn.visibility = View.INVISIBLE
+            }else{
+                binding.insertTodoBtn.visibility = View.VISIBLE
+                binding.deleteTodoBtn.visibility = View.VISIBLE
+                binding.updateTodoBtn.visibility = View.VISIBLE
+            }
+        }
+    }
+
+
+    //설명 : 일정 보기
+    private fun showTodo(text: String){
+        binding.outputTerminal.append(text + "\n")
+    }
+
+
+    //설명 : 텍스트 편집기 초기화
+    private fun clearEditTexts(){
+        with(binding){
+            editID.setText("")
+            editTodo.setText("")
+        }
+    }
+
+
+    //설명 : 날짜를 클릭하여 그 날짜에 일정을 추가
+    private fun addTodo(){
+        binding.insertTodoBtn.setOnClickListener {
+            try{
+                databaseHelper.insertData(
+                    binding.outputDate.text.toString().trim(),
+                    binding.editTodo.text.toString().trim()
+                )
+                clearEditTexts()
+                showTodo("일정이 추가됨")
+            }catch(e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    //설명 : 날짜를 누르고 id와 일정을 입력하면 기존 일정이 변경됨
+    private fun updateTodo(){
+        binding.updateTodoBtn.setOnClickListener {
+            try{
+                databaseHelper.updateData(
+                    binding.editID.text.toString().trim(),
+                    binding.editTodo.text.toString().trim()
+                )
+                clearEditTexts()
+                showTodo("일정이 변경됨")
+            }catch(e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    //설명 : 날짜를 선택하고 id 번호를 입력하면 그 id에 해당하는 일정이 삭제됨
+    private fun removeTodo(){
+        binding.deleteTodoBtn.setOnClickListener {
+            try{
+                databaseHelper.deleteData(binding.editID.text.toString().trim())
+                clearEditTexts()
+                showTodo("일정이 삭제됨")
+            }catch(e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    //설명 : 선택된 날짜와 관련된 모든 일정 가져오기
+    private fun getAllTodo(){
+        binding.viewTodoBtn.setOnClickListener {
+             try{
+                 val selectResult = databaseHelper.getAllData(binding.outputDate.text.toString().trim())
+                 showTodo(selectResult)
+             }catch(e: Exception){
+                 e.printStackTrace()
+             }
+        }
+    }
 }
